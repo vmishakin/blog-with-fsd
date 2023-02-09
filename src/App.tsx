e0472287a1f1ff92621b304eaaae5065b3c1feb8
Vadim Sachev <vs@trillion.jewelry>
2023-02-09 23:29:39 +0300

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import { Counter } from "./components/Counter";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
-      <Link to={""}>Главная</Link>
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,19 @@
+type Mods = Record<string, boolean | string>;
+
+export function classNames(
+  cls: string,
+  mods: Mods,
+  additional: string[]
+): string {
+  return [
+    cls,
+    ...additional,
+    Object.entries(mods)
+      .filter(([, value]) => Boolean(value))
+      .map(([className]) => className),
+  ].join(" ");
+}
+
+classNames("remove-btn", { hovered: true, selecable: true, red: true }, [
+  "pdg",
+]);
