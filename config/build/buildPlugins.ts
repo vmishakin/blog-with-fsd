0f992e1674493b528b7f981f27ea42900d492d12
Vadim Sachev <vs@trillion.jewelry>
2023-02-14 03:26:30 +0300

    HMR

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,13 +1,14 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpack, { DefinePlugin } from "webpack";
+import webpack, { DefinePlugin, HotModuleReplacementPlugin } from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -20,4 +21,10 @@ export function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev),
     }),
   ];
+
+  if (isDev) {
+    plugins.push(new ReactRefreshPlugin());
+  }
+
+  return plugins;
 }
