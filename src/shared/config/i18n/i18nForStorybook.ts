e0ca361db8dd8a161e9bbf8a6c50633d6ce9bbc5
Vadim Sachev <vs@trillion.jewelry>
2023-02-27 04:23:12 +0300

    add screenshot test report, fix i18n for storybook

--- src/shared/config/i18n/i18nForStorybook.ts
+++ src/shared/config/i18n/i18nForStorybook.ts
@@ -1,16 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+import aboutNS from '../../../../public/locales/en/about.json';
+import translationNS from '../../../../public/locales/en/translation.json';
 
 i18n
-  .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    lng: 'en',
     fallbackLng: 'en',
     debug: true,
-    ns: ['translation', 'about'],
+    resources: { en: { translation: translationNS, about: aboutNS } },
   });
 
 export default i18n;
