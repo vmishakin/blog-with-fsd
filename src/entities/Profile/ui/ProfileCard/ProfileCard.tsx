8ae0a87f466affb8e8dc69d083d38a0d71ee239c
Vadim Sachev <vs@trillion.jewelry>
2023-03-12 03:44:54 +0300

    lint fix

--- src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -3,16 +3,12 @@ import { useSelector } from 'react-redux';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Text } from 'shared/ui/Text/Text';
 import { Input } from 'shared/ui/Input/Input';
-import { getProfileIsLoading } from '../../model/selectors/getProfileIsLoading/getProfileIsLoading';
-import { getProfileError } from '../../model/selectors/getProfileError/getProfileError';
 import { getProfileData } from '../../model/selectors/getProfileData/getProfileData';
 import s from './ProfileCard.module.scss';
 
 export const ProfileCard = () => {
   const { t } = useTranslation('profile');
   const data = useSelector(getProfileData);
-  const isLoading = useSelector(getProfileIsLoading);
-  const error = useSelector(getProfileError);
 
   return (
     <div className={s.ProfileCard}>
