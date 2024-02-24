f333f5e8ae64436f66a7cd5e1b19a288ffbbc9ca
Vadim Mishakin <vadmishakin@gmail.com>
2024-02-25 02:41:56 +0300

    add helpers for slice, selectors, actions

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,27 +1,27 @@
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
 import { Button } from '@/shared/ui/Button';
-import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
-import { counterActions } from '../model/slice/counterSlice';
+import { useCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { useCounterActions } from '../model/slice/counterSlice';
 
 export const Counter = () => {
-  const dispatch = useDispatch();
-  const counterValue = useSelector(getCounterValue);
+  const counterValue = useCounterValue();
+  const { add, increment, decrement } = useCounterActions();
   const { t } = useTranslation();
 
-  const increment = () => {
-    dispatch(counterActions.increment());
+  const handleIncrement = () => {
+    increment();
   };
 
-  const decrement = () => {
-    dispatch(counterActions.decrement());
+  const handleDecrement = () => {
+    decrement();
   };
 
   return (
     <div>
       <h1 data-testid="value-title">{counterValue}</h1>
-      <Button onClick={increment} data-testid="increment-btn">{t('increment')}</Button>
-      <Button onClick={decrement} data-testid="decrement-btn">{t('decrement')}</Button>
+      <Button onClick={handleIncrement} data-testid="increment-btn">{t('increment')}</Button>
+      <Button onClick={handleDecrement} data-testid="decrement-btn">{t('decrement')}</Button>
+      <Button onClick={() => add(5)}>{t('add five')}</Button>
     </div>
   );
 };
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,13 @@
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
+import { Counter } from '@/entities/Counter';
 
 export const MainPage = () => {
   const { t } = useTranslation();
 
   return (
     <Page>
+      <Counter />
       <p>{t('Main page')}</p>
     </Page>
   );
