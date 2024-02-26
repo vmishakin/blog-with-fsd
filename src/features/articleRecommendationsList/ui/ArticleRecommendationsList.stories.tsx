4c32341c6e847d547118963afc20abbfbf11b15a
Vadim Mishakin <vadmishakin@gmail.com>
2024-02-26 16:26:23 +0300

    another fix stories

--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList.stories.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList.stories.tsx
@@ -6,7 +6,8 @@ import { ArticleRecommendationsList } from './ArticleRecommendationsList';
 
 const article: Article = {
   id: '1',
-  img: '',
+  // eslint-disable-next-line max-len
+  img: 'https://res.cloudinary.com/teepublic/image/private/s--HGX34F_2--/t_Preview/b_rgb:ffb81c,c_lpad,f_jpg,h_630,q_90,w_1200/v1539273850/production/designs/3302095_0.jpg',
   createdAt: '',
   views: 123,
   user: { id: '1', username: '123' },
