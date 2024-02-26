351dfe95d0a439a272e58d757b20b4f416cdc2c1
Vadim Mishakin <vadmishakin@gmail.com>
2024-02-26 14:16:12 +0300

    test commit lint-staged 2

--- src/pages/NotFound/ui/NotFoundPage.tsx
+++ src/pages/NotFound/ui/NotFoundPage.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import s from './NotFoundPage.module.scss';
- 
+
 export const NotFoundPage = () => {
   const { t } = useTranslation();
   return (
